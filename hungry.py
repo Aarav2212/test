f71817c7adb7878d44409185ab8af93769fadc18
Atul Saroj -X (asaroj - TATA CONSULTANCY SERVICES LIMITED at Cisco) <asaroj@cisco.com>
2024-02-06 00:26:05 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -4,6 +4,8 @@ hungry = input("are you hungry")
 if hungry=="yes":
     print("eat samosa")
     print("eat pizza")
+    print("eat fries")
+    print('eat burger')
 
 else:
     print("do your home work")
